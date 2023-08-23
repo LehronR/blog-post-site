2188d3d7e235ca9f831e827a152810b9c6141757
Lehron <lehronrobinson@yahoo.com>
2023-08-22 21:23:33 -0400

    completed assignment

---
+++ src/main/java/com/techtalentsouth/techtalentblog/blogpost/BlogPostRepository.java
@@ -0,0 +1,6 @@
+package com.techtalentsouth.techtalentblog.blogpost;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BlogPostRepository extends CrudRepository<BlogPost, Long> {
+}
